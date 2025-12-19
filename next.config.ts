7c88fae0dbd90addf6be91e6482a80e16ba190fe
tienlm <tienlm@vtc.vn>
2025-12-19 13:38:06 +0700

    setting

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
-import type { NextConfig } from "next";
-
+import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
-  /* config options here */
+  // output: 'export',
+  output: 'standalone',
+  basePath: '', //VD: /bapchampion
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 's3-hcm-r2.s3cloud.vn',
+      },
+      {
+        hostname: 'localhost',
+      },
+    ],
+    unoptimized: true,
+    // loaderFile: "utils/next-image-config.ts",
+  },
+  // reactStrictMode: false,
 };
 
 export default nextConfig;
